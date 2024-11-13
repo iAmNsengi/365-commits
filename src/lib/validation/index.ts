bd1bc08b07cb93f13c9b70eed2d04a5f2eef5be9
Eliezer Nsengi <nsengitech@gmail.com>
2024-11-13 10:12:15 +0200

    fix: signup validation

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,10 +1,12 @@
 import * as z from "zod";
 
 export const signupValidation = z.object({
+  name: z.string().min(2, { message: "Name is yoo short" }),
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
-  password: z.string().min(2, {
+  email: z.string().min(2, { message: "Email is too short" }),
+  password: z.string().min(6, {
     message: "Password must be at least 6 characters.",
   }),
 });
