04973061f44303f897ed6a07ac7d7381f2d6406b
iAmNsengi <iamnsengi@icloud.com>
2025-02-07 21:42:52 +0200

    ft: unfound routes fix

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,11 @@ import authRoutes from "./auth.route.js";
 const router = express.Router();
 
 router.use("/api/v1/auth", authRoutes);
+router.use("*", (req, res) =>
+  res.status(404).json({
+    success: false,
+    message: "The resource you are looking for could not be found",
+  })
+);
 
 export default router;
