9f5a68f162077bbe758e53283c5531a671608695
iAmNsengi <iamnsengi@icloud.com>
2025-02-07 17:10:54 +0200

    fx: better response json for rate limits

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { loginController } from "../controllers/auth.controller.js";
+import { loginLimit } from "../utils/rateLimits.js";
 
 const router = express.Router();
 
+router.use("/login", loginLimit);
 router.route("/login").post(loginController);
 
 export default router;
--- utils/rateLimits.js
+++ utils/rateLimits.js
@@ -3,8 +3,11 @@ import rateLimit from "express-rate-limit";
 // limit max 5 login requests in 15 min
 export const loginLimit = rateLimit({
   max: 5,
-  windowMs: 15 * 60 * 100,
-  message: "Too many login attempts, try again after 15 minutes",
+  windowMs: 15 * 60 * 1000,
+  message: {
+    success: false,
+    message: "Too many login attempts, try again after 15 minutes",
+  },
   standardHeaders: true,
   legacyHeaders: true,
 });
@@ -13,7 +16,10 @@ export const loginLimit = rateLimit({
 export const reqLimit = rateLimit({
   max: 100,
   windowMs: 15 * 60 * 1000,
-  message: "Too many requests from this IP, try again after 15 minutes",
+  message: {
+    success: false,
+    message: "Too many requests from this IP, try again after 15 minutes",
+  },
   legacyHeaders: true,
   standardHeaders: true,
 });
