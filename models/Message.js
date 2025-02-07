0b4677c1fe8879c503d0f56586638f5b66aa2402
iAmNsengi <iamnsengi@icloud.com>
2025-02-07 14:54:12 +0200

    ft: add message schema

--- models/Message.js
+++ models/Message.js
@@ -5,6 +5,7 @@ const messageSchema = new mongoose.Schema(
     sender: { type: mongoose.Schema.Types.ObjectId, required: true },
     receiver: { type: mongoose.Schema.Types.ObjectId, required: true },
     message: { type: String },
+    status: { type: String, enum: ["read", "pending"], default: "pending" },
   },
   { timestamps: true }
 );
