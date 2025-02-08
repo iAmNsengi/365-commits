792ad709f55a82127f980be26e63d14b08857a43
iAmNsengi <iamnsengi@icloud.com>
2025-02-08 10:08:17 +0200

    fx: refactor generateToken to

--- utils/responseHandler.js
+++ utils/responseHandler.js
@@ -1,8 +1,8 @@
 export const successResponse = (res, statusCode, data = null) =>
   res.status(statusCode).json({ success: true, data });
 
-export const errorResponse = (err, _, res) =>
-  res.status(err?.statusCode || 500).json({
+export const errorResponse = (err, req, res, next) =>
+  res.status(err?.code === 11000 ? 409 : err?.statusCode || 400).json({
     success: false,
     message: err?.message || "An internal server error occurred",
   });
