6cc3c9864ea5676dadf1d9820608b0fe3fee8e92
iAmNsengi <iamnsengi@icloud.com>
2025-02-08 18:35:44 +0200

    ft: add cors

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import compression from "compression";
+import mongoSanitize from "express-mongo-sanitize";
+import cors from "cors";
 
 import appRoutes from "./routes/index.js";
 import { mongodbConnection } from "./config/connectDb.js";
 import { errorResponse } from "./utils/responseHandler.js";
 import { reqLimit } from "./utils/rateLimits.js";
+import corsOptions from "./utils/corsOptions.js";
 
 dotenv.config();
 const app = new express();
@@ -16,6 +19,8 @@ mongodbConnection();
 app.use(express.json());
 app.use(helmet());
 app.use(compression());
+app.use(mongoSanitize()); // data sanitization against NoSQL attacks
+app.use(cors(corsOptions));
 
 app.use(reqLimit);
 
