3b821b964201b49d3ba5cbaea746ab03b2d62b68
iAmNsengi <iamnsengi@icloud.com>
2025-02-08 10:08:35 +0200

    fx: conflict status on constraint unique

--- utils/responseHandler.js
+++ utils/responseHandler.js
@@ -1,8 +1,13 @@
 export const successResponse = (res, statusCode, data = null) =>
   res.status(statusCode).json({ success: true, data });
 
-export const errorResponse = (err, req, res, next) =>
-  res.status(err?.code === 11000 ? 409 : err?.statusCode || 400).json({
-    success: false,
-    message: err?.message || "An internal server error occurred",
-  });
+export const errorResponse = (err, req, res, next) => {
+  console.log();
+
+  return res
+    .status(err?.cause?.code === 11000 ? 409 : err?.statusCode || 500)
+    .json({
+      success: false,
+      message: err?.message || "An internal server error occurred",
+    });
+};
